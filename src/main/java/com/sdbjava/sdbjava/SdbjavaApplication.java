25a15c7fe57a22bcf151b6b37ca954a9893fe8a3
Cristiano-Paz <cristianogspaz@gmail.com>
2025-05-20 21:03:12 -0300

    commit inicial

---
+++ src/main/java/com/sdbjava/sdbjava/SdbjavaApplication.java
@@ -0,0 +1,13 @@
+package com.sdbjava.sdbjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdbjavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdbjavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sdbjava/sdbjava/SdbjavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sdbjava.sdbjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdbjavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

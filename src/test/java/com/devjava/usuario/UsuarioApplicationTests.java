bcd2fa65361d915968234411fc4d23d1fa42ad28
Cristiano-Paz <cristianogspaz@gmail.com>
2025-07-31 16:56:56 -0300

    commit inicial

---
+++ src/test/java/com/devjava/usuario/UsuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devjava.usuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsuarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

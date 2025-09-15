63fbf5b1358529c52337ff7a16b6fe7dbaa603c3
Cristiano-Paz <cristianogspaz@gmail.com>
2025-09-15 20:18:37 -0300

    refactor class security

---
+++ src/main/java/com/crisdev/usuario/business/UsuarioService.java
@@ -0,0 +1,12 @@
+package com.crisdev.usuario.business;
+
+import com.crisdev.usuario.infrastructure.repository.UsuarioRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UsuarioService {
+
+    private final UsuarioRepository usuarioRepository;
+}

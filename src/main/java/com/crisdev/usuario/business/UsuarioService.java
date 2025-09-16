4ccdba43ae6d3a1d2394170dafba75c8eecf385b
Cristiano-Paz <cristianogspaz@gmail.com>
2025-09-15 21:15:25 -0300

    added method that saves user data

--- src/main/java/com/crisdev/usuario/business/UsuarioService.java
+++ src/main/java/com/crisdev/usuario/business/UsuarioService.java
@@ -1,5 +1,8 @@
 package com.crisdev.usuario.business;
 
+import com.crisdev.usuario.business.converter.UsuarioConverter;
+import com.crisdev.usuario.business.dto.UsuarioDTO;
+import com.crisdev.usuario.infrastructure.entity.Usuario;
 import com.crisdev.usuario.infrastructure.repository.UsuarioRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -9,4 +12,10 @@ import org.springframework.stereotype.Service;
 public class UsuarioService {
 
     private final UsuarioRepository usuarioRepository;
+    private final UsuarioConverter usuarioConverter;
+
+    public UsuarioDTO salvaUsuario(UsuarioDTO usuarioDTO) {
+        Usuario usuario = usuarioConverter.paraUsuario(usuarioDTO);
+        return usuarioConverter.paraUsuarioDTO(usuarioRepository.save(usuario));
+    }
 }
